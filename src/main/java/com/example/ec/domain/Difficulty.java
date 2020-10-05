8d5e76afb30c819953969534384fdf0c15fbb94f
gjavierbcn <72359886+gjavierbcn@users.noreply.github.com>
2020-10-05 23:30:19 +0200

    Add files via upload

---
+++ src/main/java/com/example/ec/domain/Difficulty.java
@@ -0,0 +1,11 @@
+package com.example.ec.domain;
+
+/**
+ * Enumeration of the level of effort.
+ *
+ * Capstone Full Stack Developer with React.
+ * Gustavo Javier Garrido.
+ */
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies;
+}
